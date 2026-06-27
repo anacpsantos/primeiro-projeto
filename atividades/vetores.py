1782effb420fea534d5e5b60c8e83233dbabc912
anacpsantos <aanacpsantos@gmail.com>
2026-06-26 21:14:09 -0300

    atividade 26/06

---
+++ atividades/vetores.py
@@ -0,0 +1,15 @@
+alunos = ["Augusto", "Alisson", "Ana", "Isabelle", "João","Wesley"]
+print(alunos[0])
+print(alunos[1])
+print(alunos[2])
+print(alunos[3])
+print(alunos[4])
+print(alunos[5])
+
+numeros=[1,2,3,4,5]
+print(numeros[0])
+print(numeros[1])
+print(numeros[2])
+print(numeros[3])
+print(numeros[4])
+
